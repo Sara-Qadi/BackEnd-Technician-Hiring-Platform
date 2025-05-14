66aacc0abb841200021b06bdf510bf9124743150
SHADY SAWALHA <s12323788@stu.najah.edu>
2025-05-14 23:19:08 +0300

    add seededrs

---
+++ database/seeders/failed_jobsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class failed_jobsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
