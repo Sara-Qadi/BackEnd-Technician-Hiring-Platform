466798def6b6ecb6b41d82c325ea368fcaec736b
Omar Safarini <omarsafarini123344@gmail.com>
2025-06-07 12:11:57 +0300

    masseging backend feature and fixing erros

--- database/factories/JobpostFactory.php
+++ database/factories/JobpostFactory.php
@@ -23,34 +23,6 @@ class JobPostFactory extends Factory
             'location' => $this->faker->city,
             'description' => $this->faker->paragraph,
             'user_id' => User::inRandomOrder()->first()->user_id,
-            'title' =>fake()->randomElement([
-                'fixing my kitchen',
-                'painting the walls',
-                'moving furniture',
-                'installing shelves',
-                'repairing the roof'
-            ]),
-            'category' =>fake()->randomElement([
-                'Carpenter',
-                'Plumber',
-                'Electrician',
-                'Painter',
-                'Mason',
-                'Roofing',
-                'Mechanic',
-                'Welder',
-                'Tiler',
-                'ACTechnician',
-                'CameraInstaller'
-            ]),
-            'maximum_budget' => 100,
-            'minimum_budget' => 10,
-            'deadline' => fake()->dateTimeBetween('now', '+1 year'),
-            'status' => fake()->randomElement(['pending', 'in progress', 'completed']),
-            //'attachments' => fake()->imageUrl(),
-            'location' => fake()->city(),
-            'description' => fake()->paragraph(),
-            'user_id' => fake()->numberBetween(1, 7),
         ];
     }
 }
